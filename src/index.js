3d1646d0ac2a546544d7a2f3fd2129ef4ba32c12
github-SergeyRbv <serzryabov14@gmail.com>
2023-01-24 12:43:42 +0300

    docs:add info to index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const { reverse } = require("ramda");
+
 module.exports = function reverse (n) {
-  
+    let newString = '';
+
+  for (let i = n.length - 1; i >= 0; i--) {
+    newString += n[i];
+  }
+  return Number(newString);
 }
