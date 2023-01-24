7cecfe05df4fac19924888e60eb0d68ac5811490
github-SergeyRbv <serzryabov14@gmail.com>
2023-01-24 17:43:23 +0300

    fixed: correct the errors

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
-const { reverse, mathMod } = require("ramda");
-
 module.exports = function reverse (n) {
+    
     let number = Math.abs(n)
     let result = number.toString().split("").reverse().join("");
     return result;
